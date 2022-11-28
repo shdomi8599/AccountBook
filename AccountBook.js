e9c711f5c60ad4c00e35ebeed4d21a40e34659f0
Dongmin Shin <shdomi@naver.com>
2022-11-28 20:58:13 +0900

    + css,navi

--- AccountBook.js
+++ AccountBook.js
@@ -1,9 +1,18 @@
 const td = document.querySelectorAll('td');
 
-for (let tds of td) {
-    let answer = 1;
- tds.innerText = answer;
-    answer++; 
+const th = document.querySelector('tbody').querySelectorAll('th');
+
+let answer = 1;
+
+//td를 1부터 35까지 채운 값
+for (let tds of th) {
+    tds.innerText = answer;
+    answer++;
 }
 
-console.log(td);
+//요일이 31일까지이기 때문에 32~35의 값을 지운 것 
+for (let i = 31; i <= 34; i++) {
+    th[i].innerText = '';
+}
+
+//날짜 호출기 제작해보자
